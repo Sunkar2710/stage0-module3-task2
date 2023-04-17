ccd7cc0f21efc84b09523c831fd68ca4ae59655b
Sunkar <sunkar2710@gmail.com>
2023-04-17 23:19:26 +0600

    fix: fix variable names

--- src/main/java/lang/print/gaps/task2/CopiedNames.java
+++ src/main/java/lang/print/gaps/task2/CopiedNames.java
@@ -2,13 +2,13 @@ package lang.print.gaps.task2;
 
 public class CopiedNames {
     public static void main(String[] args) {
-        int variable = 0;
-        int variable = 2000;
-        int value = 1000;
-        int variable = 4000;
-        int value2 = 3000;
-        int variables = 5000;
-        variables = 7000;
-        int value = 90000;
+        int variableZero = 0;
+        int variableTwoThousand = 2000;
+        int valueOneThousand = 1000;
+        int variableFourThousand = 4000;
+        int valueThreeThousand = 3000;
+        int variableFiveThousand = 5000;
+        int variableSevenThousand = 7000;
+        int valueNinetyThousand = 90000;
     }
 }
